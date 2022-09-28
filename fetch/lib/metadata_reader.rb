1c72831dfa877a752f683bc59d5745db18ad4fe9
Jumpei Matsuda <jmatsu.drm@gmail.com>
2022-09-28 23:08:27 +0900

    feat: add a fallback rescue while processing URLs
    includes documentation changes

--- fetch/lib/metadata_reader.rb
+++ fetch/lib/metadata_reader.rb
@@ -4,20 +4,24 @@ class MetadataReader
   # @param file [File] an opened file. this class does not close it so be careful.
   def initialize(file)
     @html = Nokogiri::HTML(file)
-    @site = File.basename(file.path.gsub(/\.html\z/, ''))
+    @site = File.basename(file.path.gsub(/\.html\z/, '')) # trust the naming convention
     @last_fetch = file.mtime
   end
 
   # @return [Hash] a metadata of a content
+  #    site: [String] URL
+  #    num_links: the number of a tags which have href attributes. They may contain invalid/expired links.
+  #    images: the number of img tags which have src attributes. They may contain invalid/expired links.
+  #    last_fetch: when the file was fetched. No modification by other than this script is expected
   def read
     images = @html.search('//img').reject { |d| d.attr('src')&.empty? }.count
     num_links = @html.search('//a').reject { |d| d.attr('href')&.empty? }.count
 
     {
-    site: @site,
-    num_links: num_links,
-    images: images,
-    last_fetch: @last_fetch
+      site: @site,
+      num_links: num_links,
+      images: images,
+      last_fetch: @last_fetch
     }
   end
 end
